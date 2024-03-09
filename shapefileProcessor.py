e848d15ae25e048f7e2412dc7d3cc3281589ea33
Diarmaid Hehir <diarmaidhehir@gmail.com>
2024-03-09 22:05:45 +0000

    Added "No data" display for areas where data is not available

--- shapefileProcessor.py
+++ shapefileProcessor.py
@@ -1,38 +1,50 @@
 import geopandas as gpd
 import pandas
 from matplotlib import pyplot as plt
+from matplotlib.patches import Patch
 
 
+def displayFile(dataframe, removed_rows, output, feature):
+    # Plot language data
+    ax = dataframe.plot(column=feature, cmap='Greens', linewidth=0.1, edgecolor='0.8', legend=True)
 
-def displayFile(dataframe, output, feature):
-    # Plot the GeoDataFrame with specified feature column
-    dataframe.plot(column=feature, cmap='Greens', linewidth=0.8, edgecolor='0.8', legend=True)
-    plt.axis('off')
-    # Set DPI(dots per inch) to get high resolution image and save it to specified directory
+    # Plot removed districts where no data is present with grey color
+    if not removed_rows.empty:
+        removed_rows.plot(ax=ax, color='grey', linewidth=0.1, edgecolor='0.8', alpha=0.5, label='No data')
+
+    # Hide axes
+    ax.axis('off')
+
+    # Create legend and add to plot
+    legend_elements = [Patch(facecolor='grey', edgecolor='grey', alpha=0.5, label='No data')]
+    ax.legend(handles=legend_elements, loc='lower left')
+
+    # Set DPI(dots per inch) to get high resolution image and save to specified directory
     plt.savefig(output, dpi=800, bbox_inches='tight')
-    # Show the plot
+
+    # Show plot
     plt.show()
 
 
 def processFile(shapeDF, speakersDF):
-    # Create a set of unique GUIDs from speakersDF
+    # get GUIDs from speakersDF
     unique_guids = set(speakersDF['GUID'])
 
-    # Filter shapeDF to remove rows with GUID not present in speakersDF (coos, loughatorick, etc)
+    # Filter shapeDF to remove rows that are not present in speakersDF (no data)
     filtered_shapeDF = shapeDF[shapeDF['GUID'].isin(unique_guids)]
 
-    # removed_rows = shapeDF[~shapeDF['GUID'].isin(unique_guids)]
+    removed_rows = shapeDF[~shapeDF['GUID'].isin(unique_guids)]
     # for index, row in removed_rows.iterrows():
     #     print("Removed row:", row)
 
     # Merge speakersDF with filtered_shapeDF on GUID
     merged_df = filtered_shapeDF.merge(speakersDF, on='GUID', how='left')
 
-    return merged_df
+    return merged_df, removed_rows
 
 shapeDF = gpd.read_file(r".\shapefiles\Galway_ED_20M.geojson")
 speakersDF = pandas.read_csv('Regressor_Prediction.csv')
-dataframe = processFile(shapeDF, speakersDF)
+dataframe, removed_rows = processFile(shapeDF, speakersDF)
 for index, row in dataframe.iterrows():
     print(row)
-displayFile(dataframe, r'.\maps\output_map.png',"Speakers2022")
+displayFile(dataframe,removed_rows, r'.\maps\output_map.png',"Speakers2022")
